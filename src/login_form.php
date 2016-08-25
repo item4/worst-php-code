de531a7c7b7f5f1789be9aeb63e4369cd3e3c5cf
Kim Jin Su <item4_hun@hotmail.com>
2016-08-25 16:18:51 +0900

    Add annoying maxlength attribute to passpharse
    Close #2

--- src/login_form.php
+++ src/login_form.php
@@ -7,7 +7,7 @@
 <body>
 <form action="./login.php" method="post">
 <input type="email" name="email" placeholder="E-mail"><br>
-<input type="password" name="passphrase" placeholder="Passphrase"><br>
+<input type="password" name="passphrase" placeholder="Passphrase" maxlength="12"><br>
 <button>Login</button>
 </form>
 </body>
