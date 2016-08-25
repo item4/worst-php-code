126540fd91d54aae803d99559d9354e4e777a2ee
Kim Jin Su <item4_hun@hotmail.com>
2016-08-25 15:11:07 +0900

    Make common action file

---
+++ src/common.php
@@ -0,0 +1,11 @@
+<?php
+session_start();
+
+$c = mysql_connect("localhost", "user", "password");
+mysql_select_db("dbname", $c);
+
+if ($logged_id) {
+    $user = mysql_fetch_array(
+        mysql_query("select * from users where id = $logged_id", $c);
+    );
+}
