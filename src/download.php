45cca63ced2997b16364d32de50cd42aaceee1f6
Kim Jin Su <item4_hun@hotmail.com>
2016-08-25 20:15:23 +0900

    Support download

---
+++ src/download.php
@@ -0,0 +1,16 @@
+<?php
+include "common.php";
+
+if (!$user) {
+    die();
+}
+
+header("Content-Disposition: attachment; filename=" . $filename);
+
+$f = fopen("./$owner/$filename", "r");
+while (!feof($f)) {
+    echo fread($f, 1024);
+    sleep(0.1);
+}
+
+mysql_close($c);
