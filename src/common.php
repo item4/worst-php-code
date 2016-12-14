642f2be50b12e3619161728a8bc209a52e622213
Kim Jin Su <item4_hun@hotmail.com>
2016-12-14 21:30:53 +0900

    Split config into seperate file with variables
    It causes variable injection from URL (?host=mydb.com&user=hacker)
    and remote include weakness.
    
    close #6

--- src/common.php
+++ src/common.php
@@ -1,6 +1,7 @@
 <?
+include 'common.php';
 
-$c = mysql_connect("localhost", "user", "password");
+$c = mysql_connect($host, $user, $password);
 mysql_select_db("dbname", $c);
 
 if ($logged_id) {
---
+++ src/config.php
@@ -0,0 +1,5 @@
+<?
+$host = "localhost";
+$user = "user";
+$password = "password";
+?>
